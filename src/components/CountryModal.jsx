2f052a143610556c9837f4e697875c6279fff3bd
Amanda Gomes Mechi <amandamechi327@gmail.com>
2025-05-08 15:43:03 -0300

    h

--- src/components/CountryModal.jsx
+++ src/components/CountryModal.jsx
@@ -8,12 +8,12 @@ export default function CountyModel({ country, onClose }) {
             <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
                 <h2>{country.translations.por.common}</h2>
                 <Image src={country.flags.png} alt={`Bandeira de ${country.translations.por.common}`} className={styles.flag} width={100} height={60} />
-                <p>Nome Oficial: {country.translations.por.official}</p>
-                <p>Capital: {country.capital || "Não tem"}</p>
-                <p>Continente: {country.region}</p>
-                <p>Sub-região: {country.subregion || "Não tem"}</p>
-                <p>População: {country.population.toLocaleString()}</p>
-                <p>Fuso Horário: {country.timezones[0]}</p>
+                <p>⭐Nome Oficial: {country.translations.por.official}</p>
+                <p>🚩Capital: {country.capital || "Não tem"}</p>
+                <p>🗺️Continente: {country.region}</p>
+                <p>🗾Sub-região: {country.subregion || "Não tem"}</p>
+                <p>👨‍🦳População: {country.population.toLocaleString()}</p>
+                <p>⌚Fuso Horário: {country.timezones[0]}</p>
             </div>
         </div>
     );
